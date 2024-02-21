e08fc7b2ac8a0b16fed5d99abe3821e519e21241
Marcel Martinez Otano <m.otano@omsag.de>
2024-02-21 16:15:16 +0100

    User Registration

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserController;
 use App\Models\Listing;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -47,3 +48,9 @@ Route::delete('/listings/{listing}', [ListingController::class, 'destroy']);
 //show single listing
 //listing parameter -> Route model binding
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+//show register create form
+Route::get('/register', [UserController::class, 'create']);
+
+//create new user
+Route::post('/users', [UserController::class, 'store']);
