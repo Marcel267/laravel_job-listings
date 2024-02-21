7b7087a0057d133190d02203dcd49ddf8a9d60ab
Marcel Martinez Otano <m.otano@omsag.de>
2024-02-21 10:09:15 +0100

    Using a Layout Component

--- resources/views/listings/show.blade.php
+++ resources/views/listings/show.blade.php
@@ -1,5 +1,4 @@
-@extends('layout')
-@section('content')
+<x-layout>
     @include('partials/_search')
     @if ($listing)
         <a href="/" class="inline-block text-black ml-4 mb-4"><i class="fa-solid fa-arrow-left"></i> Back
@@ -41,4 +40,4 @@
     @else
         <div>error: listing not found</div>
     @endif
-@endsection
+</x-layout>
